a8353618d45061e2e04f76131fddf58428541576
uknow-mac <yoon6763@naver.com>
2023-11-18 00:21:24 +0900

    chapter2: 파싱 로직 리팩토링

---
+++ chapter2-3-deposit-and-withdrawal/src/main/BankTransaction.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import static java.util.Objects.hash;
+
+public class BankTransaction {
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+    public BankTransaction(LocalDate date, double amount, String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" +
+                "date=" + date +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BankTransaction that = (BankTransaction) obj;
+        return Double.compare(that.amount, amount) == 0 &&
+                date.equals(that.date) &&
+                description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, amount, description);
+    }
+}
---
+++ chapter2-3-deposit-and-withdrawal/src/main/Main.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(String[] args) throws Exception {
+
+        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+
+        final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, 1));
+    }
+
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        double total = 0d;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final int month) {
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth().getValue() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
+    }
+}
