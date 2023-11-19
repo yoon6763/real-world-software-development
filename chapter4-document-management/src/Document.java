d07e11a92350214f77d8c9c3a0e3ba78d9693f69
uknow-home <yoon6763@naver.com>
2023-11-20 01:36:37 +0900

    complete chapter 4

--- chapter4-document-management/src/Document.java
+++ chapter4-document-management/src/Document.java
@@ -3,11 +3,11 @@ import java.util.Map;
 public class Document {
     private final Map<String, String> attributes;
 
-    public Document(Map<String, String> attributes) {
+    Document(final Map<String, String> attributes) {
         this.attributes = attributes;
     }
 
-    public String getAttribute(String attributeName) {
+    public String getAttribute(final String attributeName) {
         return attributes.get(attributeName);
     }
 }
--- chapter4-document-management/src/ImageImporter.java
+++ chapter4-document-management/src/ImageImporter.java
@@ -5,16 +5,17 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ImageImporter implements Importer {
 
+class ImageImporter implements Importer {
     @Override
     public Document importFile(final File file) throws IOException {
         final Map<String, String> attributes = new HashMap<>();
-
         attributes.put(PATH, file.getPath());
 
         final BufferedImage image = ImageIO.read(file);
-
+        attributes.put(Attributes.WIDTH, String.valueOf(image.getWidth()));
+        attributes.put(Attributes.HEIGHT, String.valueOf(image.getHeight()));
+        attributes.put(Attributes.TYPE, "IMAGE");
 
         return new Document(attributes);
     }
