c1119f39c4e5f8ca0109f1f15b168dc3026a194f
uknow-home <yoon6763@naver.com>
2023-11-19 23:58:22 +0900

    입출력 파서 인터페이스 및 구현체

--- chapter2-3-deposit-and-withdrawal/src/main/Application.java
+++ chapter2-3-deposit-and-withdrawal/src/main/Application.java
@@ -13,22 +13,10 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
 
-        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
 
-        final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
-        final List<String> lines = Files.readAllLines(path);
-
-        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
-        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
-
-        collectSummary(bankStatementProcessor);
-    }
-
-    private static void collectSummary(BankStatementProcessor bankStatementProcessor) {
-        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
-        System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
-        System.out.println("The total for transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
-        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
     }
 
 }
---
+++ chapter2-3-deposit-and-withdrawal/src/main/BankStatementParser.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.List;
+
+public interface BankStatementParser {
+
+    BankTransaction parseFrom(String line);
+
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+
+}
